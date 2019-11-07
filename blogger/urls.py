38be9d6ab392d247b123e763df66fb3ce6bacae8
Mudit Bhardwaj <muditbhardwaj195@gmail.com>
2019-11-07 20:08:47 +0530

    Add Frontend auth

--- blogger/urls.py
+++ blogger/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', indexView, name='index')
+    path('', indexView, name='index'),
+    path('login/', logInView, name='logIn'),
+    path('category/', categoryView, name='category'),
+    path('contact/', contactView, name='contact'),
+    path('about/', aboutView, name='about'),
+    path('blogsingle/', blogSingleView, name='blog-single'),
+    path('signup/', signUpView, name='signup')
 ]
--- blogger/views.py
+++ blogger/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def indexView(request):
     return render(request,'index.html')
+
+def logInView(request):
+    return render(request,'login.html')
+
+def aboutView(request):
+    return render(request,'about.html')
+
+def categoryView(request):
+    return render(request,'category.html')
+
+def contactView(request):
+    return render(request,'contact.html')
+
+def blogSingleView(request):
+    return render(request,'blog-single.html')
+
+def signUpView(request):
+    return render(request,'signup.html')
